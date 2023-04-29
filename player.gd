a5f01f9bd8d7f72dfc1ef5a23ae1d262b8a16dfd
Kira <kafountain5@gmail.com>
2023-04-29 11:57:53 -0400

    player movement

--- player.gd
+++ player.gd
@@ -1,37 +1,53 @@
-extends CharacterBody2D
+extends CharacterBody3D
 
 
-@export var speed = 200
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
 
+@onready var sprite = $AnimatedSprite3D
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-	
-	
-func get_input():
-	var input_direction = Input.get_vector("Left", "Right", "Up", "Down")
-	velocity = input_direction * speed
-	print(input_direction)
-	# right
-	if input_direction == Vector2(1,0):
-		$AnimatedSprite2D.play("side")
-		$AnimatedSprite2D.flip_h = true
-	# left
-	elif input_direction == Vector2(-1,0):
-		$AnimatedSprite2D.play("side")
-		$AnimatedSprite2D.flip_h = false
-	# up
-	elif input_direction == Vector2(0, -1):
-		$AnimatedSprite2D.play("down")
-	# down
-	elif input_direction == Vector2(0,1):
-		$AnimatedSprite2D.play("down")
-		
-		
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
 
 func _physics_process(delta):
-	get_input()
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("Left", "Right", "Up", "Down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	# Handle Jump.
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		sprite.play("jump")
+	else:
+		if direction: 
+			if is_on_floor():
+				sprite.play("walk")
+			else:
+				sprite.play("jump")
+			
+		
+			if direction.x < 0:
+				sprite.flip_h = true
+			else:
+				sprite.flip_h = false
+					
+			velocity.x = direction.x * SPEED
+			velocity.z = direction.z * SPEED
+		
+		else:
+			sprite.stop()
+			if is_on_floor():
+				sprite.play("idle")
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	print(velocity)
 	move_and_slide()
-
-
