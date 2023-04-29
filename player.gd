ef82da26ee13f6d08ab0e7d37ab68c7e30259b9b
shinykiwi <kafountain5@gmail.com>
2023-04-29 06:56:25 -0400

    first

--- player.gd
+++ player.gd
@@ -1,11 +1,37 @@
-extends Area2D
+extends CharacterBody2D
+
+
+@export var speed = 200
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+	
+	
+func get_input():
+	var input_direction = Input.get_vector("Left", "Right", "Up", "Down")
+	velocity = input_direction * speed
+	print(input_direction)
+	# right
+	if input_direction == Vector2(1,0):
+		$AnimatedSprite2D.play("side")
+		$AnimatedSprite2D.flip_h = true
+	# left
+	elif input_direction == Vector2(-1,0):
+		$AnimatedSprite2D.play("side")
+		$AnimatedSprite2D.flip_h = false
+	# up
+	elif input_direction == Vector2(0, -1):
+		$AnimatedSprite2D.play("down")
+	# down
+	elif input_direction == Vector2(0,1):
+		$AnimatedSprite2D.play("down")
+		
+		
+
+func _physics_process(delta):
+	get_input()
+	move_and_slide()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
---
+++ world.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	
+
+func camera_pos():
+	
+	var camera = $Camera2D
+	var viewport = get_viewport().size
+	camera.limit_top = 0
+	camera.limit_left = 0
+	camera.limit_right = viewport.x
+	camera.limit_bottom = viewport.y
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
